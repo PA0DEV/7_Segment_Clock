5232b3faad52b4f87c0977b9d938373a9d730391
PA0DEV <phillip.ahlers25@gmail.com>
2021-10-11 21:54:25 +0200

    Implementation der FW Check URL

--- scripts/main.ino
+++ scripts/main.ino
@@ -1,3 +1,5 @@
 const char* fwVersion = "0.0.1";
-const char* fwUrlBase = ""
+const char* fwUrlBase = "https://raw.githubusercontent.com/PA0DEV/7_Segment_Clock/7cd281ff75800fd96a23fabe00cac83e16a6d96e/scripts/latest.version?token=AV5RLCXF4WCRWE22UAEZDFDBMRKZ2"
+
+
 
