8c2abee89258a5362ae33d2edb13644895bfac74
PA0DEV <phillip.ahlers25@gmail.com>
2021-10-17 01:39:10 +0200

    added HTTPS Request for Firmware check

--- scripts/main.ino
+++ scripts/main.ino
@@ -1,5 +1,65 @@
-const char* fwVersion = "0.0.1";
-const char* fwUrlBase = "https://raw.githubusercontent.com/PA0DEV/7_Segment_Clock/7cd281ff75800fd96a23fabe00cac83e16a6d96e/scripts/latest.version?token=AV5RLCXF4WCRWE22UAEZDFDBMRKZ2"
+
+// Include Libaries
+#include <Arduino.h>
+#include <ESP8266HTTPClient.h>
+#include <ESP8266WiFi.h>
+#include <WiFiClientSecure.h>
+// #include <ESP8266httpUpdate.h>
 
 
+// Setup Wifi Objects
+WiFiClientSecure client;
+HTTPClient https;
 
+// Declare global variables
+const String fwVersion = "0.0.1";
+const String fwUrl = "https://raw.githubusercontent.com/PA0DEV/7_Segment_Clock/main/scripts/latest.version";
+const char* ssid = "PATH to Internet";
+const char* pass = "WLANphtasBUDE";
+IPAddress ip(192, 168, 178, 200);
+IPAddress sub(255, 255, 255, 0);
+IPAddress gw(192, 168 ,178, 1);
+IPAddress dns(8, 8, 8, 8);
+IPAddress dns2(8, 8, 4, 4);
+
+void setup(){
+    
+    Serial.begin(115200);
+    Serial.println("Starting");
+    WiFi.mode(WIFI_STA);
+    
+    
+    WiFi.begin(ssid, pass);
+    WiFi.config(ip, gw, sub, dns);
+    Serial.printf("Waiting for Connect");
+    while (WiFi.status() != WL_CONNECTED){
+       Serial.printf(".");
+       delay(500);
+    };
+    Serial.println(WiFi.localIP());
+    Serial.print(WiFi.dnsIP());
+   Serial.println();
+    
+};
+
+void loop(){
+    
+    Serial.print("\nChecking Updates...");
+    checkFwUpdate(); 
+    delay(5000);   
+};
+
+void checkFwUpdate(){
+    /*Check for newest FW Version in the git repository (on fw Url)
+    */
+   client.setInsecure();
+   client.connect("https://raw.githubusercontent.com", 443);
+
+   https.begin(client, fwUrl);
+   String payload;
+   if (https.GET() == 200){
+       payload = https.getString();
+       Serial.printf("Payload: %s", payload);
+   };
+   Serial.printf("\nHTTP code: %i", https.GET());
+   };
